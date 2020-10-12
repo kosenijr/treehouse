e399e3969a4f2e3ea7e2da07ddeb032c9f1f994b
Kayode Oseni <k.oseni.jr@gmail.com>
2020-10-12 19:09:56 -0400

    updating template-literals

---
+++ js/template-literals.js
@@ -0,0 +1,9 @@
+const name = prompt('What is your name?');
+
+// In order to use template literals, apply back ticks (`) instead of quotations (" " or '') 
+
+const message = `Hello ${name}. Welcome to my tech site.
+I'm happy that you came by to visit, ${name}.
+Please come again to observe better updates in the future!`;
+
+console.log(message);
