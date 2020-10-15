791a1d09ed940b6536c9ce926126444e5ba5309d
Kayode Oseni <k.oseni.jr@gmail.com>
2020-10-15 03:14:14 -0400

    conditionals.js lesson is completed

---
+++ js/conditionals.js
@@ -0,0 +1,27 @@
+// // No semicolon at the end of the conditional statement.
+// const answer = prompt('Which planet is closest to the sun?');
+
+// if ( answer === 'Mercury' ) {
+//     console.log("That's Correct!")
+// } else {
+//     console.log('Sorry, that\'s incorrect.')
+// }
+// // no semicolon after else statement
+
+
+// ***************************************************************
+// No semicolon at the end of the conditional statement.
+const answer = prompt('Which planet is closest to the sun?');
+
+if ( answer.toUpperCase() === 'MERCURY' ) {
+    console.log("That's Correct!")
+} else {
+    console.log('Sorry, that\'s incorrect.')
+}
+// no semicolon after else statement
+
+// TAKEAWAYS
+// ***** When a letter or word is compared (<. =, >) to another, the first letter is greater if it alphabetically comes after the other. (i.e. ('gifts' > 'boisterous', answer: true. 'g' is greater bc it comes after 'b'.));
+
+
+
