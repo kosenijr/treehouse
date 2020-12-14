611579399d1848799a31fbd805177dbdaba909f6
Kayode Oseni <k.oseni.jr@gmail.com>
2020-12-14 02:21:12 -0500

    random6.js is modified to include preliminary activities to functions: parameters and arguments

--- JavaScript/js/random6.js
+++ JavaScript/js/random6.js
@@ -1,22 +1,34 @@
-// ***** customary for functions to be placed at the top of the file.
-// function alertRandom() {
-function getRandomNumber() {    
-    // creates a random number from 1-6 and stores it in the randomNumber variable
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
-    // Math.Floor and Math.random are both functions ( "()" )
-    // alert(randomNumber);
+// // ***** customary for functions to be placed at the top of the file.
+// // function alertRandom() {
+// function getRandomNumber() {
+//     // creates a random number from 1-6 and stores it in the randomNumber variable
+//     const randomNumber = Math.floor(Math.random() * 6) + 1;
+//     // Math.Floor and Math.random are both functions ( "()" )
+//     // alert(randomNumber);
+//     return randomNumber;
+// }
+// // alertRandom();
+// // alertRandom();
+// // alertRandom();
+// // alertRandom();
+
+// alert(getRandomNumber());
+// console.log(getRandomNumber());
+
+// //*** a function call ( "()" ) can be stored in variables
+// const dieRoll = getRandomNumber();
+
+
+// -------------------------------------------------
+// FUNCTION PARAMETERS AND ARGUMENTS
+function getRandomNumber(upper) { //"upper" is the parameter   
+    const randomNumber = Math.floor(Math.random() * upper) + 1;
     return randomNumber;
 }
-// alertRandom();
-// alertRandom();
-// alertRandom();
-// alertRandom();
-
-alert(getRandomNumber());
-console.log(getRandomNumber());
-
-//*** a function call ( "()" ) can be stored in variables
-const dieRoll = getRandomNumber();
+console.log( getRandomNumber(6)); // argument is "6"
+console.log( getRandomNumber(100)); // argument is "100"
+console.log( getRandomNumber(1000)); // argument is "1000"
+console.log( getRandomNumber(20)); // argument is "20"
 
 
 
